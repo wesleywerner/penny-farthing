d3d2a59f78b85cec02282f26e88feeb2ea276f46
Wesley <wesley.werner@gmail.com>
2016-05-12 22:42:07 +0200

    Move game initialization to the ui file

--- js/ui.js
+++ js/ui.js
@@ -15,14 +15,38 @@
     ui.infoEl = document.getElementById('game-info');
     ui.reset();
     
+    // This hook collapses the navbar after clicking a link,
+    // but only if the nav is expanded. This hides the nav bar nicely
+    // on mobile devices after link clicks.
+    $(document).on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
+
     // handler to set the game number
     var el = document.getElementById('set-game-number');
     el.addEventListener("click", function() {
         var el = document.getElementById('game-number');
-        initialize(null, parseInt(el.value));
+        game.ui.initialize(null, parseInt(el.value));
       });
     
   });
+  
+  
+  /**
+   * Start a new game, or restart the current game if null name is given.
+   * A specific seed can be given too, or one will be generated otherwise.
+   */
+  ui.initialize = function(gamename, newseed) {
+    // prompt on restart
+    if (!gamename) {
+      if (!confirm('Restart your game?')) return;
+    }
+    var el = document.getElementById('playfield');
+    game.controller.initialize(el, gamename, newseed);
+    game.ui.displayGameNumber(game.controller.seed);
+  };
   
   /**
    * Reset elements.
@@ -49,7 +73,7 @@
     gameNames.forEach(function(name) {
       var anchor = document.createElement('a');
       anchor.innerHTML = name.toUpperCase();
-      anchor.setAttribute("onclick", "initialize('"+name+"')");
+      anchor.setAttribute("onclick", "game.ui.initialize('"+name+"')");
       anchor.setAttribute("href", "#");
       var listItem = document.createElement('li');
       listItem.appendChild(anchor);
